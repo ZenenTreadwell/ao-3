b53300856b1c646fe282716a22887bc4ab7c77c7
taylor <>
2021-01-22 16:25:34 -0800

    bitcoinconfigy

--- install/sample_configuration.sh
+++ install/sample_configuration.sh
@@ -1,7 +1,11 @@
+AUTHDEETS=$(python3 ./rpcauth.py ao)
+AUTHLINE=$(echo $AUTHDEETS | grep -o rpcauth=ao:[^[:space:]]*[[:space:]])
+PASSLINE=$(echo $AUTHDEETS | grep -o [^[:space:]]*\$)
+echo $AUTHLINE >> $HOME/.bitcoin/bitcoin.conf
 
 echo "module.exports = {"
 echo "openAo: true,"
-echo "bitcoind: { username: '', password: ''},"
+echo "bitcoind: { username: 'ao', password: '"$PASSLINE"'},"
 echo "sqlite3: {file: '"$HOME"/.ao/database.sqlite3'},"
 echo "tor: { hostname: '"`cat /var/lib/tor/ao/hostname`"'},"
 echo "clightning: {dir: '"$HOME"/.lightning/bitcoin'},"
