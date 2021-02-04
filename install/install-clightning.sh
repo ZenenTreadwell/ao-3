38810dc161177c3262f165e2a9b7b1b1d982772b
taylor <>
2021-02-04 10:29:18 -0800

    bossy

--- install/install-clightning.sh
+++ install/install-clightning.sh
@@ -6,7 +6,23 @@ then
 else
 	git clone https://github.com/ElementsProject/lightning.git
 	cd lightning
+	git checkout v0.9.3
 	./configure
 	make
 	sudo make install
+	cd ..
+fi
+
+
+if [ $(clboss --version 2>/dev/null | grep -c "0") -eq 1 ];
+then
+	echo clboss already installed
+else
+	git clone https://github.com/ZmnSCPxj/clboss.git
+	cd clboss
+	git checkout v0.10
+	./configure
+	make
+	sudo make install
+	cd ..
 fi
