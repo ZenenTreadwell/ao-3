2034836269a3eb2a7d0e38513b9e3d09962c53ca
Your Name <you@example.com>
2023-02-24 13:48:25 -0800

    index

--- src/components/Priorities.vue
+++ src/components/Priorities.vue
@@ -84,7 +84,7 @@ export default {
   },
   computed:{
       priorities(){
-          return this.$store.getters.contextCard.priorities.slice().reverse()
+          return this.$store.getters.contextCard.priorities
       },
   },
   components:{
